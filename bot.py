2641c5c54c7d2b9b84884765689a6a84c0cdd6b0
Reuben <70382344+Keen2Kode@users.noreply.github.com>
2025-12-06 20:31:29 +1100

    separate main bot class from jam_pollls

--- bot.py
+++ bot.py
@@ -8,6 +8,8 @@ from datetime import datetime, timedelta
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.cron import CronTrigger
 from jam_commands import JamCommands
+from jam_polls import JamPolls
+from channels import Channels
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
@@ -27,29 +29,18 @@ async def on_ready():
     print(f'{bot.user} is ready')
 
     await bot.add_cog(JamCommands(bot))
+    await bot.add_cog(JamPolls(bot))
     scheduler = AsyncIOScheduler(timezone=ZoneInfo("Australia/Melbourne"))
-    common_job_kwargs = {
-        "replace_existing": True,
-        "misfire_grace_time": 60
-    }
 
-    scheduler.add_job(jam_poll, CronTrigger(day_of_week='mon', hour=12, minute=0), id="jam_poll", **common_job_kwargs)
-    scheduler.add_job(card_holder_poll, CronTrigger(day_of_week='mon', hour=12, minute=0), id="card_holder_poll", **common_job_kwargs)
-    scheduler.add_job(card_holder_alert_1, CronTrigger(day_of_week='thu', hour=18, minute=0), id="card_holder_alert_1", **common_job_kwargs)
-    scheduler.add_job(card_holder_alert_2, CronTrigger(day_of_week='fri', hour=20, minute=0), id="card_holder_alert_2", **common_job_kwargs)
-
-    # await jam_poll()
-    # await card_holder_poll()
-    # await card_holder_alert_1()
-    # await card_holder_alert_2()
-    
+    jam_polls = bot.get_cog("JamPolls")
+    await jam_polls.add_scheduled_polls(scheduler)
     scheduler.start()
 
 
 
 @bot.check
 async def globally_block_commands(ctx):
-    if ctx.channel and ctx.channel in bot_channels():
+    if ctx.channel and ctx.channel in Channels(bot).bot_channels():
         return True
     raise commands.CheckFailure("Please use this bot only in the designated bot channels.")
 
@@ -61,101 +52,6 @@ async def on_command_error(ctx, error):
     else:
         raise error  # Let other errors surface normally
     
-
-
-
-
-
-
-
-async def card_holder_alert_1():    
-    print(f'card holder alert 1 entered')
-    card_holder_channel = jam_channel('card-holders')
-
-    answers_empty_flags = await no_card_holder_day_answers(card_holder_channel)
-
-    if answers_empty_flags is None:
-        await card_holder_channel.send("No active Card Holder poll found.")
-        return
-    
-    for answer in answers_empty_flags:
-        await card_holder_channel.send(f"No one is available on {answer.text}!"
-        " A message will be sent to the relevant jam channel on Friday if no one is available.")
-    
-async def card_holder_alert_2():
-    print(f'card holder alert 2 entered')
-    card_holder_channel = jam_channel('card-holders')
-
-    answers_empty_flags = await no_card_holder_day_answers(card_holder_channel)
-
-    if answers_empty_flags is None:
-        await card_holder_channel.send("No active Card Holder poll found.")
-        return
-    
-    channels = []
-    for answer in answers_empty_flags:
-        if "Saturday" in answer.text:
-            channels.append(jam_channel('rock'))
-            channels.append(jam_channel('pop'))
-        if "Sunday" in answer.text:
-            channels.append(jam_channel('jazz'))  
-
-    for channel in channels:
-        await channel.send(f"Staff is unavailable to open the space for this week. Please reach out to the SBS team for confirmation.")
-
-
-async def card_holder_poll():
-    print(f'card holder poll entered')
-    card_holder_channel = jam_channel('card-holders')
-
-    p = discord.Poll(
-        question=f"Card Holders, are you available to open the space at the following times?",
-        duration=timedelta(days=5)
-    )
-    p.add_answer(text='Saturday 12pm')
-    p.add_answer(text='Sunday 12pm')
-    await card_holder_channel.send(poll=p)    
-
-async def jam_poll():
-    print(f'jam poll entered')
-    mapping = {}
-    mapping['rock'] = (jam_channel('rock'), "Saturday (3-6pm)")
-    mapping['jazz'] = (jam_channel('jazz'), "Sunday (12-3pm)")
-    mapping['pop'] = (jam_channel('pop'), "Saturday (12-3pm)")
-
-    for name, (channel, time) in mapping.items():
-        p = discord.Poll(
-            question=f"Are you attending the {name.capitalize()} Jam this {time}? If YES, specify your instrument below for priority.",
-            duration=timedelta(days=5)
-        )
-        for instrument in ["Guitar", "Piano", "Drums", "Bass", "Singing", "Other"]:
-            p.add_answer(text=instrument)
-        await channel.send(poll=p)
-
-
-
-
-
-async def no_card_holder_day_answers(card_holder_channel: discord.TextChannel) -> list[discord.PollAnswer] | None:
-    async for message in card_holder_channel.history(limit=500):
-        if message.author == bot.user and message.poll and not message.poll.is_finalized():
-            return [answer for answer in message.poll.answers if answer.vote_count==0]
-    return None
-
-def jam_channel(channel_name):
-    for guild in bot.guilds:
-        for channel in guild.text_channels:
-            if channel.category and channel.category.name == "Jams" and channel.name == channel_name:
-                    return channel
-    return None
-
-def bot_channels():
-    channels = []
-    for guild in bot.guilds:
-        for channel in guild.text_channels:
-            if channel.name.startswith("bot"):
-                channels.append(channel)
-    return channels
 
 
 
---
+++ jam_polls.py
@@ -0,0 +1,148 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Context, bot
+from datetime import datetime, timedelta
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.cron import CronTrigger
+from jam_commands import JamCommands
+from channels import Channels
+
+# load_dotenv()
+# TOKEN = os.getenv("DISCORD_TOKEN")
+
+# intents = discord.Intents.default()
+# intents.message_content = True  # Required to read message content
+# intents.guilds = True
+# intents.guild_messages = True
+# intents.members = True
+
+# bot = commands.Bot(command_prefix='!', intents=intents)
+
+class JamPolls(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.channels = Channels(bot)
+
+
+
+
+    @commands.command(name="jampoll")
+    async def jam_poll_cmd(self, ctx: Context):
+        if ctx.message.author.guild_permissions.administrator:
+            await self.jam_poll()
+
+    @commands.command(name="committeepoll")
+    async def card_holder_poll_cmd(self, ctx: Context):
+        if ctx.message.author.guild_permissions.administrator:
+            await self.card_holder_poll()
+
+    @commands.command(name="cardalert1")
+    async def card_holder_alert_1_cmd(self, ctx: Context):
+        if ctx.message.author.guild_permissions.administrator:
+            await self.card_holder_alert_1()
+
+    @commands.command(name="cardalert2")
+    async def card_holder_alert_2_cmd(self, ctx: Context):
+        if ctx.message.author.guild_permissions.administrator:
+            await self.card_holder_alert_2()
+
+
+
+
+    async def add_scheduled_polls(self, scheduler: AsyncIOScheduler):
+        common_job_kwargs = {
+            "replace_existing": True,
+            "misfire_grace_time": 60
+        }
+
+        scheduler.add_job(self.jam_poll, CronTrigger(day_of_week='mon', hour=12, minute=0), id="jam_poll", **common_job_kwargs)
+        scheduler.add_job(self.card_holder_poll, CronTrigger(day_of_week='mon', hour=12, minute=0), id="card_holder_poll", **common_job_kwargs)
+        scheduler.add_job(self.card_holder_alert_1, CronTrigger(day_of_week='thu', hour=18, minute=0), id="card_holder_alert_1", **common_job_kwargs)
+        scheduler.add_job(self.card_holder_alert_2, CronTrigger(day_of_week='fri', hour=20, minute=0), id="card_holder_alert_2", **common_job_kwargs)
+
+        return scheduler
+
+
+
+    async def card_holder_poll(self):
+            print(f'card holder poll entered')
+            card_holder_channel = self.channels.committee()
+
+            p = discord.Poll(
+                question=f"Card Holders, are you available to open the space at the following times?",
+                duration=timedelta(days=5),
+                multiple=True
+            )
+            p.add_answer(text='Saturday 12pm')
+            p.add_answer(text='Sunday 12pm')
+            await card_holder_channel.send(poll=p)    
+
+    async def jam_poll(self):
+        print(f'jam poll entered')
+        mapping = {}
+        mapping['rock'] = (self.channels.jam('rock'), "Saturday (3-6pm)")
+        mapping['jazz'] = (self.channels.jam('jazz'), "Sunday (12-3pm)")
+        mapping['pop'] = (self.channels.jam('pop'), "Saturday (12-3pm)")
+
+        for name, (channel, time) in mapping.items():
+            p = discord.Poll(
+                question=f"Are you attending the {name.capitalize()} Jam this {time}? If YES, specify your instrument below for priority.",
+                duration=timedelta(days=5)
+            )
+            for instrument in ["Guitar", "Piano", "Drums", "Bass", "Singing", "Other"]:
+                p.add_answer(text=instrument)
+            await channel.send(poll=p)
+
+
+
+    async def card_holder_alert_1(self):    
+        print(f'card holder alert 1 entered')
+        card_holder_channel = self.channels.committee()
+
+        answers_empty_flags = await self.no_card_holder_day_answers(card_holder_channel)
+
+        if answers_empty_flags is None:
+            await card_holder_channel.send("No active Card Holder poll found.")
+            return
+        
+        for answer in answers_empty_flags:
+            await card_holder_channel.send(f"No one is available on {answer.text}!"
+            " A message will be sent to the relevant jam channel on Friday if no one is available.")
+        
+    async def card_holder_alert_2(self):
+        print(f'card holder alert 2 entered')
+        card_holder_channel = self.channels.committee()
+
+        answers_empty_flags = await self.no_card_holder_day_answers(card_holder_channel)
+
+        if answers_empty_flags is None:
+            await card_holder_channel.send("No active Card Holder poll found.")
+            return
+        
+        channels = []
+        for answer in answers_empty_flags:
+            if "Saturday" in answer.text:
+                channels.append(self.channels.jam('rock'))
+                channels.append(self.channels.jam('pop'))
+            if "Sunday" in answer.text:
+                channels.append(self.channels.jam('jazz'))  
+
+        for channel in channels:
+            await channel.send(f"Staff is unavailable to open the space for this week. Please reach out to the SBS team for confirmation.")
+
+
+   
+
+
+
+
+
+    async def no_card_holder_day_answers(self, card_holder_channel: discord.TextChannel) -> list[discord.PollAnswer] | None:
+        async for message in card_holder_channel.history(limit=500):
+            if message.author == self.bot.user and message.poll and not message.poll.is_finalized():
+                return [answer for answer in message.poll.answers if answer.vote_count==0]
+        return None
+
+
+
