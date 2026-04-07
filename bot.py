48e6fc8575e3d4590070fda04b7f1af8cf1effdc
Reuben <70382344+Keen2Kode@users.noreply.github.com>
2026-04-08 00:10:23 +1000

    test with 10s loop window

--- bot.py
+++ bot.py
@@ -61,7 +61,7 @@ class JamBot(commands.Bot):
     async def on_ready(self):
         print(f'{self.user} is ready')
 
-    @tasks.loop(hours=12)
+    @tasks.loop(seconds=10)
     async def schedule_jobs_loop(self):
         if not self.is_ready():
             return
